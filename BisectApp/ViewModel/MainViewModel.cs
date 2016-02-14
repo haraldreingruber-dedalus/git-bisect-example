494783f532a26e2ea7eeb40987527062c10e460c
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2016-02-14 13:57:49 +0100

    Created Main ViewModel and Locator

---
+++ BisectApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace BisectApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public RelayCommand ButtonCommand { get; set; }
+
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (value.Equals(_text))
+                    return;
+                _text = value;
+                RaisePropertyChanged(() => Text);
+            }
+        }
+
+        public MainViewModel()
+        {
+            ButtonCommand = new RelayCommand(() =>
+            {
+                Text = "Hello git enthousiast!";
+            });
+        }
+    }
+}
---
+++ BisectApp/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace BisectApp.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel MainViewModel => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            Register();
+        }
+
+        private void Register()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+    }
+}
