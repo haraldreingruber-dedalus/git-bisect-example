28975e5b352881157d46da3da609729750b00dfe
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2016-02-14 19:37:38 +0100

    Addes script used for testing

---
+++ Scripts/bisect.ps1
@@ -0,0 +1,10 @@
+﻿$content = Get-Content ../BisectApp/ViewModel/MainViewModel.cs
+
+if ($content -like "*Hello svn enthousiast!*") {
+    Write-Host "Found crappy content"
+    exit 1
+}
+else {
+    Write-Host "Probably great content"
+    exit 0
+}
